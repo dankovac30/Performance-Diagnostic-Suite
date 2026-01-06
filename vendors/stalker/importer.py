cbab97e10a96c5c4c6f04df8e9e777772a29eb27
dankovac30 <dankovac30@gmail.com>
2026-01-06 23:14:12 +0100

    feat: add Stalker ATS processing pipeline

---
+++ vendors/stalker/importer.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import os
+
+def load_stalker_rda(file_path: str, sampling_rate: float = 46.875) -> pd.DataFrame:
+    """
+    Loads raw velocity data from a Stalker radar file.
+
+    Args:
+        file_path (str): The absolute or relative path to the source file.
+        sampling_rate (float, optional): The radar's data acquisition frequency in Hz.
+                                         Defaults to 46.875 Hz (standard Stalker ATS setting).
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing 'time' and 'raw_speed'
+    """
+    raw_values = []
+
+    # Open the file in read mode
+    with open(file_path, 'r', encoding='utf-8') as f:
+
+        for line in f:
+            # Clean the line
+            line_clean = line.strip().replace(',', '.')
+
+            # Skip empty lines
+            if not line_clean:
+                continue
+            
+            # Convert the string to a float.
+            try:
+                value = float(line_clean)
+                raw_values.append(value)
+            
+            # Skip error line
+            except ValueError:
+                continue
+    
+    # Dataframe construction
+    df = pd.DataFrame(raw_values, columns=['raw_speed'])
+
+    dt = 1.0 / sampling_rate
+    df['time'] = df.index * dt
+
+    df = df[['time', 'raw_speed']]
+    
+    return df
---
+++ vendors/stalker/preprocessing.py
@@ -0,0 +1,137 @@
+from sprint_science.profiler import SprintProfilation
+from core.signal_processing import apply_butterworth_filter, find_speed_plateau
+import pandas as pd
+import numpy as np
+
+
+def crop_start(spatiotemporal_data: pd.DataFrame, time_shift: float=2.0) -> pd.DataFrame:
+    """
+    Detects the main acceleration phase based on maximum velocity gain and crops 
+    pre-movement noise or slow onset.
+
+    Args:
+        spatiotemporal_data (pd.DataFrame): DataFrame containing 'time' and speed columns.
+        time_shift (float, optional): The duration of the sliding window used to detect the surge in velocity.
+
+    Returns:
+        pd.DataFrame: The cropped DataFrame starting from the onset of significant acceleration, with time reset to 0.0s.
+    """
+    # Calculate the sampling interval
+    dt = spatiotemporal_data['time'].diff().mean()
+    
+    # Convert time_shift into number of rows
+    row_shift = int(round(time_shift / dt))
+
+    # Apply filter (2Hz) to determine the trend without noise
+    butter_filter = apply_butterworth_filter(spatiotemporal_data, data_to_smooth='median_speed', padding='symmetric', cutoff_freq=2)
+    spatiotemporal_data['smooth_speed'] = butter_filter
+
+    # Calculate velocity gain during time_shift time window
+    delta_v = spatiotemporal_data['smooth_speed'].shift(-row_shift) - spatiotemporal_data['smooth_speed']
+
+    # Find the index where the velocity gain is highest
+    max_delta_v_idx = delta_v.idxmax()
+
+    # Crop data starting from that index
+    cropped_spatiotemporal_data = spatiotemporal_data.loc[max_delta_v_idx:].copy()
+    
+    # Reset the index and reset time
+    cropped_spatiotemporal_data = cropped_spatiotemporal_data.reset_index(drop=True)
+    cropped_spatiotemporal_data['time'] = cropped_spatiotemporal_data['time'] - cropped_spatiotemporal_data['time'].iloc[0]
+    
+    # Delete temporary smooth column
+    cropped_spatiotemporal_data = cropped_spatiotemporal_data.drop('smooth_speed', axis=1)
+
+    return cropped_spatiotemporal_data
+
+
+def reconstruct_start(spatiotemporal_data):
+    """
+    Reconstructs the missing initial acceleration phase using the mono-exponential Morin sprint Model.
+
+    Args:
+        spatiotemporal_data (pd.DataFrame): The input data, starting at a non-zero velocity.
+
+    Returns:
+        pd.DataFrame: A complete dataset with synthetic start data prepended, starting at 0.0s and 0.0 m/s.
+    """
+    # Calculate the sampling interval
+    dt = spatiotemporal_data['time'].diff().mean()
+
+    # Cutoff post peak speed on filtered array
+    smooth_speed_data = apply_butterworth_filter(spatiotemporal_data, data_to_smooth='median_speed', cutoff_freq=1.3)
+    speed_plateau_idx = find_speed_plateau(smooth_speed_data)
+
+    # Acceleration phase only
+    acceleratin_data = spatiotemporal_data.iloc[:speed_plateau_idx].copy()
+    
+    # Initialize Profiler (using dummy weight/height as fit_morin only needs kinematics)
+    profiler = SprintProfilation(acceleratin_data, height=85, weight=190)
+
+    # Fit Morin's model to extract macroscopic kinematic parameters
+    _, _, params = profiler.fit_morin(acceleratin_data['median_speed'], acceleratin_data['time'])
+    
+    tau = params['Tau']
+    v_max = params['V_max']
+
+    # Get the velocity of the first available frame
+    init_speed = spatiotemporal_data['median_speed'].iloc[0]
+
+    # Calculate 'missing time' using the inverse Morin equation
+    missing_time = -tau * np.log(1 - (init_speed / v_max))
+
+    # If missing time is less than one frame just re-zero and return
+    if missing_time < dt:
+        spatiotemporal_data['time'] = spatiotemporal_data['time'] - spatiotemporal_data['time'].iloc[0]
+        return spatiotemporal_data
+    
+    # Generate time steps for the missing past
+    synthetic_time = np.arange(-missing_time, -dt/2, dt)
+
+    # Calculate synthetic velocities for these time steps
+    real_time = synthetic_time + missing_time
+    synthetic_speed = v_max * (1 - np.exp(-real_time / tau))
+
+    # Create DataFrame for the synthetic data
+    synthetic_data = pd.DataFrame({
+        'time': synthetic_time,
+        'median_speed': synthetic_speed
+    })
+
+    # Stitch synthetic history with real measurements and re-zero
+    full_data = pd.concat([synthetic_data, spatiotemporal_data], ignore_index=True)
+    full_data['time'] = full_data['time'] - full_data['time'].iloc[0]
+    
+    return full_data
+
+
+def crop_end(spatiotemporal_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Trims the end of the dataset based on significant velocity drop-off.
+
+    Args:
+        spatiotemporal_data (pd.DataFrame): The full run data.
+
+    Returns:
+        pd.DataFrame: The dataset trimmed at the point of significant deceleration.
+    """
+    # Smooth data to find the top speed
+    smooth_speed_data = apply_butterworth_filter(spatiotemporal_data, data_to_smooth='median_speed', cutoff_freq=1.3)
+    top_speed_idx = find_speed_plateau(smooth_speed_data)
+    top_speed = smooth_speed_data[top_speed_idx]
+
+    # Analyze only the data agter the top speed
+    post_top_speed_data = smooth_speed_data[top_speed_idx:]
+    
+    # Create a mask where speed drops below 80% of top speed
+    mask = post_top_speed_data < top_speed * 0.8
+    
+    # Find the first index where this condition is met
+    if np.any(mask):
+        fatigue_idx = np.argmax(mask) + top_speed_idx
+    else: fatigue_idx = None
+
+    # Slice the dataframe up to the detected end point
+    spatiotemporal_data = spatiotemporal_data.loc[:fatigue_idx]
+
+    return spatiotemporal_data
